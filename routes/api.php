a8e01e4cef53745112191999278a61125d607539
marydcrz <maryangelinedelacruz107@gmail.com>
2023-10-31 23:16:34 +0800

    routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,8 +10,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
@@ -18,3 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// get todos list
+Route::get('/todos', [TodoController::class, 'index']);
+// retrieve a single todo
+Route::get('/todos/{id}', [TodoController::class, 'show']);
+// create a todo
+Route::post('/todos', [TodoController::class, 'store']);
+// update a todo
+Route::put('/todos/{id}', [TodoController::class, 'update']);
+// delete a todo
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
